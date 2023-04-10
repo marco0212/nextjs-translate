5aba996501e09818227964b6f30e9ab0e4b7f00d
marco0212 <marco@dwhale.kr>
2023-04-10 09:33:02 +0900

    fix: 테스트를 보완한다

--- cypress/e2e/post-detail.cy.ts
+++ cypress/e2e/post-detail.cy.ts
@@ -1,13 +1,12 @@
 describe("Post Detail Page", () => {
   const postId = 1;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     cy.visit(`/posts/${postId}`);
   });
 
   it("go to list 버튼 클릭 시 리스트 페이지로 이동한다", () => {
-    // cy.url().should("include", "/posts/");
-    cy.contains("go to list");
-    // cy.url().should("not.include", "/posts/");
+    cy.contains("go to list").click();
+    cy.contains("Fucking Welcome Home Page");
   });
 });
